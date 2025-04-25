47add7861ad76c0216acc97db2fa9c44bdf9e0b3
powwwy <maxkimani12@gmail.com>
2025-04-25 08:40:31 +0300

    Task 1. Completed, Explanations in README

--- python-practice/README.md
+++ python-practice/README.md
@@ -1,2 +1,9 @@
 152531 - Maxwell Muthumbi
 ICS 2C
+
+Task 1. Sum all elements in a list
+
+I opted to use user input to add a fresh twist to the code. 
+First allowing the user to set the number of items they want.
+Then allowing the user to add the numbers to the list.
+Then summing the items.
---
+++ python-practice/sum_of_list.py
@@ -0,0 +1,7 @@
+mySum = [] #making an empty list to allow user input
+x = int(input("Enter a range:")) #letting the user set the amount of numbers they want
+for i in range(x):
+    num = int(input("Enter Number: "))
+    mySum.append(num) #adding each new number to the list
+summy = sum(mySum) #using the in built sum function to add the elements  
+print("The sum of items in your list = " + summy)
