cd76d2ef361d2baf43bbfd65071acaeaf3fad726
powwwy <maxkimani12@gmail.com>
2025-04-26 19:18:37 +0300

    fixed spelling errors

--- python-practice/sum_of_digits.py
+++ python-practice/sum_of_digits.py
@@ -1,7 +1,7 @@
 def taskSix():#function declaration
-    digits = list(input("Enter a number to get the sum of its didgits: ")) #user inout
+    digits = list(input("Enter a number to get the sum of its digits: ")) #user input
     sum = 0 #initialise sum to 0
-    while digits: #while the listy of digits is not empty
+    while digits: #while the list of digits is not empty
         sum += int(digits.pop()) #loop through the list of digits, set them as integers and from the last one add them
-    print (str(sum)) #display resuls
+    print (str(sum)) #display results
 taskSix() #function call
